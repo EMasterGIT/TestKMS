d2359856eeefba176f4a92ad1df2220a0709f93e
Elar Meister <elar.meister@tptlive.ee>
2021-10-01 16:21:32 +0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
- var_dump("Test");
+
+
+spl_autoload_register(function ($class){
+    $class = str_replace('App\\', '', $class);
+    require __DIR__ . "/../src/$class.php";
+});
+
+require __DIR__ . "/../routes.php";
+
+$router = new App\Router($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+$match = $router->match();
+
+if($match != null){
+    call_user_func($match['action']);
+} else {
+    echo "404";
+}
+// switch($_SERVER['REQUEST_URI']){
+//     case '/':
+//         echo "Home Page";
+//         break;
+//     case '/about':
+//         echo "About us";
+//         break;
+//     default: 
+//         echo "404";
+//         break;
+// }
